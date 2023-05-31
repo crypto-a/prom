bfe95be0c089300713b1b4ab88ab72f75d684e6e
Ali Rahbar <72822222+crypto-a@users.noreply.github.com>
2023-05-31 00:27:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function moveButton() {
-    var button = document.querySelector('.btn-no');
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-  
-    button.style.left = x + 'px';
-    button.style.top = y + 'px';
-  }
+  var button = document.querySelector('.btn-no');
+  var rect = button.getBoundingClientRect();
+
+  var minX = 15;
+  var maxX = window.innerWidth - rect.width - 15;
+  var minY = 15;
+  var maxY = window.innerHeight - rect.height - 15;
+
+  var x = Math.random() * (maxX - minX) + minX;
+  var y = Math.random() * (maxY - minY) + minY;
+
+  button.style.left = x + 'px';
+  button.style.top = y + 'px';
+}
+
   
   function acceptInvitation() {
     alert("Great! I'm looking forward to it!");
